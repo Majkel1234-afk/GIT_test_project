b9f36be0e272bf92a1fae9a345a334e68ede6a6d
Michal Kaweckigit config --global user.namegit config --global user.name Michal <kowalski4306@gmail.com>
2019-08-25 20:58:35 +0200

    commit10 - pierwszy commit po rozszczepieniu branchy mastera

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
+void funkcjaBranchyMaster()
+{
+  std::cout << "Czesc, tu funkcja rozwoju galezi mastera!" << std::endl;
+}
+
 using namespace std;
 //drobna poprawka do commita 5
 int main()
 {
-  cout << "Hello World!" << endl;
+  funkcjaBranchyMaster();
   return 0;
 }
 
