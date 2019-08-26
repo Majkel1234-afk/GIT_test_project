cd3d7f8c5b0a5e73a3c0235aa321ff853ab23768
Michal Kaweckigit config --global user.namegit config --global user.name Michal <kowalski4306@gmail.com>
2019-08-27 00:19:10 +0200

    masterBranchX - commit 1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 
-void funkcjaBranchyMaster()
-{
-  std::cout << "Czesc, tu funkcja rozwoju galezi mastera!" << std::endl;
-}
-
-using namespace std;
-//drobna poprawka do commita 5
 int main()
 {
-  funkcjaBranchyMaster();
-  std::cout << " Kolejny dzien, kolejny commit na masterze! " << std::endl;
+  std::cout << "branchaX - commit 1" << std::endl;
   return 0;
 }
 
