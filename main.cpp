347770b448e4b0cd59a8b51b993ca0fc3be14ee7
Michal Kaweckigit config --global user.namegit config --global user.name Michal <kowalski4306@gmail.com>
2019-08-27 00:22:54 +0200

    masterBranchY - commit 3

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main()
   std::cout << "branchaX - commit 3" << std::endl;
   std::cout << "branchaY - commit 1" << std::endl;
   std::cout << "branchaY - commit 2" << std::endl;
+  std::cout << "branchaY - commit 3" << std::endl;
   return 0;
 }
 
