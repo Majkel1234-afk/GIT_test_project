c218d2eee81d3446d86e807baa4c3d8d1d52d128
Majkel1234-afk <54502591+Majkel1234-afk@users.noreply.github.com>
2019-08-29 20:19:54 +0200

    cleaning main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,2 @@
-#include <iostream>
-#include <string>
 
-using namespace std;
-//'fix wrzucany na someKindOfBranch'
-int main()
-{
-  std::string someKindBranch_Message{"Czesc tu brancha 'someKind..' \nRozwijamy projekt po swojemu!"};
-  std::cout << someKindBranch_Message << std::endl;
-  return 0;
-}
 
